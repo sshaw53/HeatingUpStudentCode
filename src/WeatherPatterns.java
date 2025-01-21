5341e99315c6d725ca980e24b7a76277a3e9da99
sierrashaw <sierra.shaw@menloschool.org>
2025-01-20 21:12:08 -0800

    ahhhhh work

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -19,24 +19,25 @@ public class WeatherPatterns {
     public static int longestWarmingTrend(int[] temperatures) {
         int tempLen = temperatures.length;
         int[] days = new int[tempLen];
+        // Build the Adjacency List
+        ArrayList<Integer>[] adjList = new ArrayList[tempLen];
+        // For each node, create a list of nodes that lead to it
+        for (int i = 0; i < tempLen; i++) {
+            for (int j = i; j < tempLen; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    adjList[j].add(i);
+                }
+            }
+        }
+
+        for (int i = 0; i < tempLen; i++) {
+            int idk = longestPathTo(i, days, adjList);
+        }
+
         int longestSpan = 0;
 
         // Go through every temperature
         for (int i = 0; i < tempLen; i++) {
-            int currentTemp = temperatures[i];
-            int mostDays = 0;
-
-            // For every value up to that temperature, check to find the longest length of days that stems from
-            // the current temperature we're at (must be below current temperature to add another step)
-            for (int j = 0; j < i; j++) {
-                if (temperatures[j] < currentTemp && days[j] > mostDays) {
-                    mostDays = days[j];
-                }
-            }
-
-            // Adding one to the largest "streak"
-            days[i] = mostDays + 1;
-
             // Check as you go to find the max number of days
             if (days[i] > longestSpan) {
                 longestSpan = days[i];
@@ -46,42 +47,23 @@ public class WeatherPatterns {
         return longestSpan;
     }
 
-    /* NEW ATTEMPT
-    public static int longestWarmingTrend(int[] temperatures) {
-        int tempLen = temperatures.length;
-        int[] days = new int[tempLen];
-        int longestSpan = 0;
+    // Recursive call function
+    public static int longestPathTo(int vertex, int[] days, ArrayList<Integer>[] adjList) {
+        int len = 0;
+        int toLook = adjList[vertex].size();
 
-        Dictionary dict = new Dictionary(temperatures, tempLen);
-        ArrayList<int[]> sorted = dict.getDictionary();
-
-        // Go through every temperature
-        for (int i = 0; i < tempLen; i++) {
-            int currentTemp = temperatures[i];
-            int mostDays = 0;
-
-            // For every value up to that temperature, check to find the longest length of days that stems from
-            // the current temperature we're at (must be below current temperature to add another step)
-            for (int j = 0; j < sorted.size(); j++) {
-                int[] look = sorted.get(j);
-                if (look[0] < currentTemp && look[1] < i && days[j] > mostDays) {
-                    mostDays = days[j];
-                }
-                if (look[0] > currentTemp) {
-                    break;
-                }
+        for (int i = 0; i < toLook; i++) {
+            // Before recursing, check to see if we have been to that node already & if so, use its saved value
+            if (days[adjList[vertex].get(i)] != 0) {
+                len = days[adjList[vertex].get(vertex)];
             }
 
-            // Adding one to the largest "streak"
-            days[i] = mostDays + 1;
-
-            // Check as you go to find the max number of days
-            if (days[i] > longestSpan) {
-                longestSpan = days[i];
+            // During recursive step: If we've never been there, recurse. When done, add its new value to array.
+            else {
+                len = Math.max(len, longestPathTo(adjList[vertex].get(i), days, adjList));
             }
+            days[days[adjList[vertex].get(i)]] = 1 + len;
         }
-
-        return longestSpan;
+        return 1 + len;
     }
-*/
 }
