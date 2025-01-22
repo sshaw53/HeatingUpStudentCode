3d03432867254ab8fc028725275ca6fb040a9b04
sierrashaw <sierra.shaw@menloschool.org>
2025-01-22 10:03:42 -0800

    YAYYYYYYYYYY!

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -19,9 +19,16 @@ public class WeatherPatterns {
     public static int longestWarmingTrend(int[] temperatures) {
         int tempLen = temperatures.length;
         int[] days = new int[tempLen];
+        int longestSpan = 0;
+
         // Build the Adjacency List
         ArrayList<Integer>[] adjList = new ArrayList[tempLen];
-        // For each node, create a list of nodes that lead to it
+
+        // For each node instantiate an arrayList, then create a list of nodes that lead to it
+        for (int i = 0; i < tempLen; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
         for (int i = 0; i < tempLen; i++) {
             for (int j = i; j < tempLen; j++) {
                 if (temperatures[j] > temperatures[i]) {
@@ -30,14 +37,10 @@ public class WeatherPatterns {
             }
         }
 
+        // Call longestPathTo for each temperature in the array
         for (int i = 0; i < tempLen; i++) {
-            int idk = longestPathTo(i, days, adjList);
-        }
+            days[i] = longestPathTo(i, days, adjList);
 
-        int longestSpan = 0;
-
-        // Go through every temperature
-        for (int i = 0; i < tempLen; i++) {
             // Check as you go to find the max number of days
             if (days[i] > longestSpan) {
                 longestSpan = days[i];
@@ -50,19 +53,18 @@ public class WeatherPatterns {
     // Recursive call function
     public static int longestPathTo(int vertex, int[] days, ArrayList<Integer>[] adjList) {
         int len = 0;
+
+        // Before recursing, check to see if we have been to that node already & if so, use its saved value
+        if (days[vertex] != 0) {
+            len = days[vertex];
+            return len;
+        }
+
         int toLook = adjList[vertex].size();
 
+        // During recursive step: If we've never been there, recurse. When done, add its new value to array.
         for (int i = 0; i < toLook; i++) {
-            // Before recursing, check to see if we have been to that node already & if so, use its saved value
-            if (days[adjList[vertex].get(i)] != 0) {
-                len = days[adjList[vertex].get(vertex)];
-            }
-
-            // During recursive step: If we've never been there, recurse. When done, add its new value to array.
-            else {
-                len = Math.max(len, longestPathTo(adjList[vertex].get(i), days, adjList));
-            }
-            days[days[adjList[vertex].get(i)]] = 1 + len;
+            len = Math.max(len, longestPathTo(adjList[vertex].get(i), days, adjList));
         }
         return 1 + len;
     }
